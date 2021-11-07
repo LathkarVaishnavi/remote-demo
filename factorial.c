d8521f05e40feae10ffae28d0cea142192e39e62
Vaishnavi Lathkar <vaishnavi_lathkar@persistent.com>
2021-11-07 20:36:09 +0530

    change fact to factorial from local repo

--- factorial.c
+++ factorial.c
@@ -1,10 +1,10 @@
 # include<stdio.h>
 void main()
 {
-  int fact=1,n=5;
+  int factorial=1,n=5;
   for(int i=1;i<=5;i++)
   {
-    fact=fact*i;
+    factorial=factorial*i;
   }
-  printf("factorial is = ",fact);
+  printf("factorial is = ",factorial);
 }
